0c6d7d995faf8e48665672a1d50da3545f62c823
spena-datum <34897286+spena-datum@users.noreply.github.com>
2019-04-14 19:30:25 -0600

    Agregado las clases de lectura y cambios en el MainPage

--- YoLlegoApp/YoLlegoApp/App.xaml.cs
+++ YoLlegoApp/YoLlegoApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace YoLlegoApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
---
+++ YoLlegoApp/YoLlegoApp/Services/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace PMApp.Services
+{
+    public class Response
+    {
+        public bool IsSucess { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
