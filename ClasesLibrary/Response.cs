d981d6f202dc0decae27ea33ca8fc124dec99110
spena-datum <34897286+spena-datum@users.noreply.github.com>
2019-04-15 18:18:25 -0600

    Creado el WS para realizar la insersión en la base (sin finalizar)

---
+++ ClasesLibrary/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace ClasesLibrary
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
---
+++ WebServices/Estudiante.cs
@@ -0,0 +1,11 @@
+﻿namespace WebServices
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    class Estudiante
+    {
+        public int UsuarioId { get; set; }
+
+    }
+}
