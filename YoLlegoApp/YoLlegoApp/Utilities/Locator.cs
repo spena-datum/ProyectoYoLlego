3911ed897286f46e634009958c495b616e861b2e
spena-datum <34897286+spena-datum@users.noreply.github.com>
2019-05-20 22:46:39 -0600

    Agregada la geolocalización

--- YoLlegoApp/YoLlegoApp/Utilities/Locator.cs
+++ YoLlegoApp/YoLlegoApp/Utilities/Locator.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using YoLlegoApp.Models;
+using Xamarin.Essentials;
 
 namespace YoLlegoApp.Utilities
 {
@@ -11,16 +12,63 @@ namespace YoLlegoApp.Utilities
     {
         public async Task<Response> getLocatorData()
         {
-            //var locator = CrossGeolocator.Current;
-            //locator.DesiredAccuracy = 50;
-            //var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
-            //Location location = new Location();
-            //location.Latitud = position
-            //return new Response
-            //{
-            //    IsSucess = true,
-            //    Result = 
-            //};
+            Models.Location loc = new Models.Location();
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    return new Response
+                    {
+                        IsSucess = true,
+                        Result = location
+                    };
+                    
+                }
+                return new Response
+                {
+                    IsSucess = false,
+                    Message = $"Location is null"
+                };
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                return new Response
+                {
+                    IsSucess = false,
+                    Message = $"El dispositivo no es compatible para conocer la ubicación - {fnsEx.Message}"
+                };
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                return new Response
+                {
+                    IsSucess = false,
+                    Message = $"Ubicación no habilitada - {fneEx.Message}"
+                };
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                return new Response
+                {
+                    IsSucess = false,
+                    Message = $"No se tienen los permisos necesarios - {pEx.Message}"
+                };
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                return new Response
+                {
+                    IsSucess = false,
+                    Message = $"No se pudo obtener la ubicación - {ex.Message}"
+                };
+            }
         }
         
     }
