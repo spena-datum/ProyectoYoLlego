f560f21311fae782f36f8ecb1730c2bfb74c85a6
spena-datum <34897286+spena-datum@users.noreply.github.com>
2019-05-30 22:20:20 -0600

    Servicio realizado

---
+++ WebServices/Controllers/BaseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebServices.Controllers
+{
+    public class BaseController : ApiController
+    {
+        public string InsertarAsistencia(int fila)
+        {
+            string cadena = ConfigurationManager.ConnectionStrings["AsistenciaDBConn"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(cadena))
+            {
+                conn.Open();
+                using (SqlCommand com = new SqlCommand("ActualizarAsistencia", conn))
+                {
+                    com.CommandType = System.Data.CommandType.StoredProcedure;
+                    com.Parameters.AddWithValue("@fila", fila);
+                    com.ExecuteNonQuery();
+                }
+            }
+            return "algo";
+        }
+    }
+}
---
+++ YoLlegoApp/YoLlegoApp/Utilities/ApiActions.cs
@@ -0,0 +1,88 @@
+﻿namespace YoLlegoApp.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using YoLlegoApp.Models;
+
+    public class ApiActions
+    {
+        public async Task<Response> GetList<T>(string urlBase, string prefix, string controller)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var url = $"{prefix}{controller}";
+                var response = await client.GetAsync(url);
+                var answer = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSucess = false,
+                        Message = answer,
+                    };
+                }
+
+                var list = JsonConvert.DeserializeObject<List<T>>(answer);
+                return new Response
+                {
+                    IsSucess = true,
+                    Result = list
+                };
+            }
+            catch (Exception e)
+            {
+                return new Response
+                {
+                    IsSucess = false,
+                    Message = e.Message,
+                };
+
+            }
+        }
+
+        public async Task<Response> Post<T>(string urlBase, string prefix, string controller, T model)
+        {
+            try
+            {
+                var request = JsonConvert.SerializeObject(model);
+                var content = new StringContent(request, Encoding.UTF8, "application/json");
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var url = $"{prefix}{controller}";
+                var response = await client.PostAsync(url, content);
+                var answer = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSucess = false,
+                        Message = answer,
+                    };
+                }
+
+                var obj = JsonConvert.DeserializeObject<T>(answer);
+                return new Response
+                {
+                    IsSucess = true,
+                    Result = obj
+                };
+            }
+            catch (Exception e)
+            {
+                return new Response
+                {
+                    IsSucess = false,
+                    Message = e.Message,
+                };
+            }
+        }
+    }
+}
